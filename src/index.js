2f4f6088f6dd2506a2d6718124a2dccec635dee6
MiroRaike <129476403+MiroRaike@users.noreply.github.com>
2023-06-17 02:35:13 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+if (document.readyState !== "loading") {
+  console.log("Document is ready!");
+  HelloWorld();
+  list();
+} else {
+  document.addEventListener("DOMContentLoaded", function () {
+    console.log("Document is ready after waiting!");
+    HelloWorld();
+    list();
+  });
+}
+
+function HelloWorld() {
+  const mybutton = document.getElementById("my-button");
+
+  mybutton.addEventListener("click", function () {
+    const head = document.getElementById("Header1");
+    head.innerText = "My notebook";
+  });
+}
+
+function list() {
+  const listButton = document.getElementById("add-data");
+
+  listButton.addEventListener("click", function () {
+    const listInput = document.getElementById("listInput").value;
+    let theUl = document.getElementById("ul");
+    var item = document.createElement("li");
+    item.appendChild(document.createTextNode(listInput));
+    theUl.appendChild(item);
+  });
+}
